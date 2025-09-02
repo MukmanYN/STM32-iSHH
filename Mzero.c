13613fdc850c67e957b3de3cf45c7456ddebd35b
MukmanYN <mukhtar.suleman303@gmail.com>
2025-09-02 11:34:12 -0400

    Update Mzero.c

--- Mzero.c
+++ Mzero.c
@@ -1,18 +1,7 @@
-/* USER CODE BEGIN Header */
+tern/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+external interrupt button press using Callback functon with debounce, blinking external LED
   ******************************************************************************
   */
 /* USER CODE END Header */
@@ -235,3 +224,4 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
